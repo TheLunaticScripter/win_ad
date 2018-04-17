25c177cd97651412a80535b5d7edddab26daf8f7
John Snow <thelunaticscripter@outlook.com>
2018-04-17 15:09:41 -0400

    Remove dependency on windows cookbook
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'jsnow@chef.io'
 license 'Apache-2.0'
 description 'Resources for interacting with Active Directory'
 long_description 'Installs/Configures win_ad'
-version '3.0.7'
+version '3.0.8'
 supports 'windows'
-depends 'windows'
+
 source_url 'https://github.com/TheLunaticScripter/win_ad'
 issues_url 'https://github.com/TheLunaticScripter/win_ad/issues'
 chef_version '>= 12.7' if respond_to?(:chef_version)

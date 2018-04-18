23e5a54a3b7b2a075e879333d08ac6c17c66a21b
John Snow <thelunaticscripter@outlook.com>
2018-04-18 11:36:35 -0400

    Comment out deprecation for project
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

--- resources/client.rb
+++ resources/client.rb
@@ -18,7 +18,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-deprecated 'This resource will be deprecated with the release of Chef Client 14.x which contains the windows_ad_join resource.'
+# deprecated 'This resource will be deprecated with the release of Chef Client 14.x which contains the windows_ad_join resource.'
 
 property :domain_name, String, required: true
 property :dns_server, String
